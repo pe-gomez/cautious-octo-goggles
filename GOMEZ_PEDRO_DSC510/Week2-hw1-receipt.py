3b9f2bc3b2c9e0f347efc13e4196c5633447d018
pe-gomez <pegomez@my365.bellevue.edu>
2019-09-02 12:26:24 -0500

    hw2

---
+++ GOMEZ_PEDRO_DSC510/Week2-hw1-receipt.py
@@ -0,0 +1,9 @@
+# File: Week2-hw1-receipt.py
+# Name: Pedro E Gomez
+# Date: 2-sep-2019
+# Course: DSC510-T303 Introduction to Programming (2201-1)
+# Desc:
+# Usage:
+
+#
+print ("Hello World") # test
