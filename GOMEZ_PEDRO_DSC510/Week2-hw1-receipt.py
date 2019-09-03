547096ece12f7ee67cbb556bbbf30d6716751e32
pe-gomez <pegomez@my365.bellevue.edu>
2019-09-03 15:12:12 -0500

    hw2

--- GOMEZ_PEDRO_DSC510/Week2-hw1-receipt.py
+++ GOMEZ_PEDRO_DSC510/Week2-hw1-receipt.py
@@ -6,4 +6,27 @@
 # Usage:
 
 #
-print ("Hello World") # test
+print ("Welcome to the Installation Cost Calculator.") # Display a welcome message for your user.
+
+# Retrieve the company name from the user.
+company_name = "ACME AAA" #input("Company Name: ")
+
+# Retrieve the number of feet of fiber optic cable to be installed from the user. (convert to number)
+number_of_feet = 20.1 #float(input("Feet of cable installed:"))
+
+# Calculate the installation cost of fiber optic cable by multiplying the total cost as the number
+#   of feet times $0.87.
+intallation_cost = number_of_feet * 0.87
+
+# Print a receipt for the user including the company name, number of feet of fiber to be installed,
+#   the calculated cost, and total cost in a legible format.
+print ("\nReceipt for: %s" % (company_name))
+
+#print ("The installtion cost will be: %d * .87 = %d dollars" % (number_of_feet, intallation_cost))
+print ("The installtion cost will be: ",number_of_feet,"* 0.87 =",int(intallation_cost*100+.5)/100," dollars" )
+print (intallation_cost)
+print ("The installtion cost will be: ",number_of_feet,"* .87 =",intallation_cost," dollars" )
+
+
+#Include appropriate comments throughout the program.
+
--- GOMEZ_PEDRO_DSC510/test3.py
+++ GOMEZ_PEDRO_DSC510/test3.py
@@ -2,3 +2,4 @@ x=43
 x=x+1
 print(x)
 print(x)
+print(type('str'))
