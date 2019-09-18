2a22117cae9d2523ee2e71863bab090169b8a6bc
pe-gomez <pegomez@my365.bellevue.edu>
2019-09-17 23:42:13 -0500

    hw3

--- test2.py
+++ test2.py
@@ -1,6 +1,23 @@
+'''
 colors = ["red", "blue", "yellow", "green"]
 
 i = 0
 while i < len(colors):
     print ("when i was %d, my favorite color was %s" % (i, colors[i]))
     i=i+1
+
+import random
+
+for i in range(10):
+    x = random.random()
+    print(x)
+'''
+
+def student_info(*args,**kwargs):
+    print(args)
+    print(kwargs)
+
+courses=['math','art']
+info={'name':'John','age':22}
+
+student_info(*courses,**info)
