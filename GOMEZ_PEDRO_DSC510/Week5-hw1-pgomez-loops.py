5bf47a4262a402a3b233f7fb5ac56301361340fc
pe-gomez <pegomez@my365.bellevue.edu>
2019-09-26 21:03:17 -0500

    hw5

--- GOMEZ_PEDRO_DSC510/Week5-hw1-pgomez-loops.py
+++ GOMEZ_PEDRO_DSC510/Week5-hw1-pgomez-loops.py
@@ -14,20 +14,24 @@
 def performCalculation(myop):
     ''' parameter will be the operation being performed (+, -, *, /).  '''
     print(f"performing {myop} of two numbers")
+
     fnum = float(input(" Enter First number: "))
     snum = float(input("Enter Second number: "))
+
     if myop == '+': myres = fnum + snum
     if myop == '-': myres = fnum - snum
     if myop == '*': myres = fnum * snum
     if myop == '/': myres = fnum / snum
+
     print(f"\n{fnum} {myop} {snum} = {myres}\n")
 
-
 def calculateAverage():
-    n = 0
-    i=1
-    mytot=0
-    myentry=0
+    n = 0       #holder for number of entries
+    i = 1       #generic counter (starting from 1)
+    mytot = 0   #holder for total
+    myentry = 0 #holder for individual entry
+
+    # ask for the number of entries to average
     while n == 0:
         try:
             n = float(input("how many numbers to input: "))
@@ -37,81 +41,40 @@ def calculateAverage():
         if n != int(n) or n < 1:
             print("**** Value entered must be a positive whole number!!!")
             n = 0
-    for i in range(1,int(n)+1):
-        myentry=0
+
+    # ask for all entries to average
+    for i in range(1, int(n) + 1):
+        myentry = 0
         while myentry == 0:
             try:
-                myentry = float(input("entry #"+str(i)+": "))
+                myentry = float(input("entry #" + str(i) + ": "))
             except ValueError:
                 print("**** Value entered must be a valid quantity!!!")
                 myentry = 0
-        mytot=mytot+myentry
-    print (f"Avarage of the {n} numbers is: {mytot/n}")
+        mytot = mytot + myentry
 
+    print(f"Avarage of the {n} numbers is: {mytot / n}")
 
 import sys
 
-myop = ''
+myop = ''   #holder for action to take (initialize to blank)
 
 while myop not in ['e', 'E']:
+
+    print("\n    (+ to add, - to subtract, * to multiply, / to divide, A to Average, E to EXIT)\n")
+
     while myop not in ['+', '-', '*', '/', 'a', 'A', 'e', 'E']:
-        print("\n    (+ to add, - to subtract, * to multiply, / to divide, A to Average, E to EXIT)\n")
         myop = input("Please select operation to perform  : ")
 
-    #perform math calcs
-    if myop in ['+', '-', '*', '/']: performCalculation(myop)
+    # perform math calcs
+    if myop in ['+', '-', '*', '/']:
+        performCalculation(myop)
 
-    #perform average
-    if myop in ['a', 'A']: calculateAverage()
+    # perform average
+    if myop in ['a', 'A']:
+        calculateAverage()
 
-    #reset to go back and ask again
+    # reset to go back and ask again
     myop = ''
 
 print("\n\nHave a nice day!!!)")
-
-'''
-print("Welcome to the Installation Cost Calculator.")  # Display a welcome message for your user.
-
-# Retrieve the company name from the user.
-company_name = input("Company Name: ")
-
-# Retrieve the number of feet of fiber optic cable to be installed from the user. (convert to number)
-
-# exception handling
-try:
-    number_of_feet = float(input("Feet of FO cable to be installed: "))
-except IndexError:
-    print("**** Quantity of cable must be supplied!!!")
-    sys.exit(1)
-except ValueError:
-    print("**** Value entered must be a valid quantity!!!")
-    sys.exit(1)
-
-# input validation
-if number_of_feet <= 0:
-    print("**** Value entered must be a positive number!!!")
-    sys.exit(1)
-
-# is there a bulk discount
-if number_of_feet > 500:
-    inst_rate = 0.5
-elif number_of_feet > 250:
-    inst_rate = 0.7
-elif number_of_feet > 100:
-    inst_rate = 0.8
-else:
-    inst_rate = 0.87
-
-# Calculate the installation cost of fiber optic
-# intallation_cost = number_of_feet * inst_rate
-intallation_cost = inst_calc(number_of_feet, inst_rate)
-
-
-# Print a receipt for the user including the company name, number of feet of fiber to be installed,
-#   the calculated cost, and total cost in a legible format.
-print("==============================================")
-print("\nReceipt for:", company_name)
-print("\nFeet of cable to be installed:", number_of_feet)
-print(f"\n    Undiscounted cost would have been: {number_of_feet} * 0.87 = {round(number_of_feet * 0.87, 2)} dollars\n")
-print(f"Your actual installation cost will be: {number_of_feet} * {inst_rate} = {round(intallation_cost, 2)} dollars")
-'''
