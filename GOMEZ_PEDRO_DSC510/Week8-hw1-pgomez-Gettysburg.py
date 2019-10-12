176b35e3275240e39d538c67dc0cffd93a9c1fb4
pe-gomez <pegomez@my365.bellevue.edu>
2019-10-12 06:23:49 -0500

    hw8

---
+++ GOMEZ_PEDRO_DSC510/Week8-hw1-pgomez-Gettysburg.py
@@ -0,0 +1,53 @@
+#   File: Week6-hw1-pgomez-lists.py
+#   Name: Pedro E Gomez
+#   Date: 12-oct-2019
+# Course: DSC510-T303 Introduction to Programming (2201-1)
+#   Desc: Open the file and process each line.
+#
+# Usage: Open the file and process each line.
+# Either add each word to the dictionary with a frequency of 1 or update the word’s count by 1.
+# Nicely print the output, in this case from high to low frequency. You should use string formatting for this. (See discussion 8.3).
+# We want to achieve each major goal with a function (one function, one action). We can find four functions that need to be created.
+#
+# add_word: Add each word to the dictionary. Parameters are the word and a dictionary. No return value.
+#
+# Process_line: There is some work to be done to process the line: strip off various characters, split out the words, and so on. Parameters are a line and the dictionary. It calls the function add word with each processed word. No return value.
+#
+# Pretty_print: Because formatted printing can be messy and often particular to each situation (meaning that we might need to modify it later), we separated out the printing function. The parameter is a dictionary. No return value.
+#
+# main: We will use a main function as the main program. As usual, it will open the file and call process_line on each line. When finished, it will call pretty_print to print the dictionary.
+#
+# In the main function, you will need to open the file. We will cover more regarding opening of files next week but I wanted to provide you with the block of code you will utilize to open the file, see below.
+
+##
+
+temperatures = []
+
+my_input = ""
+
+# read until user ends
+while my_input == "":
+
+    my_input = input("Please enter temperature, (e or E to END)  : ")
+
+    # get out if the user is done
+    if my_input in ['e', 'E']: break
+
+    # check for number
+    try:
+        my_input = float(my_input)
+    except ValueError:
+        print(f"{my_input} is not a valid entry!!!")
+        my_input = ""  # always initialize to force the question
+        continue  # keep trying for a number
+
+    # append the temperature
+    temperatures.append(my_input)
+    my_input = ""  # always initialize to force the question
+
+# order the entries (evaluation done in the sort)
+temperatures.sort()
+
+print(f"\n\n               largest temperature: {temperatures[- 1]}")
+print(f"              smallest temperature: {temperatures[0]}")
+print(f"number of temperatures in the list: {len(temperatures)}")
