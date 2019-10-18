bb1f3fca5bb7cfc4d276774978b884bbfaba3240
pe-gomez <pegomez@my365.bellevue.edu>
2019-10-18 11:19:14 -0500

    hw8

--- GOMEZ_PEDRO_DSC510/Week8-hw1-pgomez-Gettysburg.py
+++ GOMEZ_PEDRO_DSC510/Week8-hw1-pgomez-Gettysburg.py
@@ -11,13 +11,21 @@
 #
 # add_word: Add each word to the dictionary. Parameters are the word and a dictionary. No return value.
 #
-# Process_line: There is some work to be done to process the line: strip off various characters, split out the words, and so on. Parameters are a line and the dictionary. It calls the function add word with each processed word. No return value.
+# Process_line: There is some work to be done to process the line: strip off various characters,
+# split out the words, and so on. Parameters are a line and the dictionary.
+# It calls the function add word with each processed word. No return value.
 #
-# Pretty_print: Because formatted printing can be messy and often particular to each situation (meaning that we might need to modify it later), we separated out the printing function. The parameter is a dictionary. No return value.
+# Pretty_print: Because formatted printing can be messy and often particular to each
+# situation (meaning that we might need to modify it later), we separated out the
+# printing function. The parameter is a dictionary. No return value.
 #
-# main: We will use a main function as the main program. As usual, it will open the file and call process_line on each line. When finished, it will call pretty_print to print the dictionary.
+# main: We will use a main function as the main program. As usual, it will open the file
+# and call process_line on each line. When finished, it will call pretty_print to print
+# the dictionary.
 #
-# In the main function, you will need to open the file. We will cover more regarding opening of files next week but I wanted to provide you with the block of code you will utilize to open the file, see below.
+# In the main function, you will need to open the file. We will cover more regarding opening
+# of files next week but I wanted to provide you with the block of code you will utilize to
+# open the file, see below.
 
 ##
 '''
@@ -53,13 +61,23 @@ print(f"              smallest temperature: {temperatures[0]}")
 print(f"number of temperatures in the list: {len(temperatures)}")
 '''
 
-word_count=dict()
-mypath='C:\\Users\\peg_o\\Desktop\\Bellevue\\DSC510-T303 Introduction to Programming\\week8program\\gettysburg.txt'
-gba_file=open(mypath,'r')
-for line in gba_file:
-    for myreplace in [',','-','1','2','3','4','5','6','7','8','9','0']:
-        line=line.replace(myreplace,'')
-    line=line.lower()
+word_count = dict()
+mypath = 'C:\\Users\\peg_o\\Desktop\\Bellevue\\DSC510-T303 Introduction to Programming\\week8program\\gettysburg.txt'
+gba_file = open(mypath, 'r')
+for line in gba_file:  # read file, line by line
+    for myreplace in ['.', ',', '-', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']:
+        line = line.replace(myreplace, '').lower()  # clean the line; set to lower case
     mywords = line.split()
+
     for myword in mywords:
-        print(myword)
+        mykey = myword.rstrip()
+
+        if mykey not in word_count.keys():
+            word_count.update({mykey: 0})
+        word_count.update({mykey : word_count.get(mykey) + 1})
+        #print("{0}  {1}".format(mykey, word_count.get(mykey)))
+print ("\n Lenght of the dictionary: {0}\n".format(len(word_count)))
+print(" {0:<18} {1}".format("Word", "Count"))
+print("{0:-<25}".format(" "))  #25 dashes
+for (mykey, myvalue) in sorted(word_count.items()):  #print sorted dictionary
+    print(" {0:<20} {1}".format(mykey, myvalue))
