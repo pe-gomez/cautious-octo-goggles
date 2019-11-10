4d5aae958ba1b104938e11e2d45a28f1ff90e4fb
pe-gomez <pegomez@my365.bellevue.edu>
2019-11-10 15:56:35 -0600

    hw12

--- GOMEZ_PEDRO_DSC510/FINAL-pgomez-weather.py
+++ GOMEZ_PEDRO_DSC510/FINAL-pgomez-weather.py
@@ -25,6 +25,106 @@ import pycountry  # fuzzy resolution of country code (https://pypi.org/project/p
 import json  # json parsing
 
 
+class clsWeather:
+
+    def __init__(self):
+        # All times are stored in UNIX format, adjusted for timezone
+
+        self.varCityName = ""
+        self.varCountryName = ""
+        self.varTimezone = 0
+        self.varSunrise = 0
+        self.varSunset = 0
+
+        self.cntNextIndex = 0  # next available index to use in lists (for appends)
+        self.dicDay2IndexXF = dict()  # day to index location in lists (cross-reference)
+
+        # Captured dayly parameters
+        #       x-referenced to the day via the dicDay2IndexXF
+        # MaxT - Hith Temp
+        # MinT - Min Temp
+        # MaxWD - First Weather description of day
+        # MinWD - Last Weather description of day
+        self.lstMaxT = []
+        self.lstMinT = []
+        self.lstLastWD = []
+        self.lstFirstWD = []
+
+        # Corresponding Timestanps of the Captured dayly parameters (in UNIX format, adjusted for timezone)
+        #       x-referenced to the day via the dicDay2IndexXF
+        self.lstMaxTUtime = []
+        self.lstMinTUtime = []
+        self.lstLastWDUtime = []
+        self.lstFirstWDUtime = []
+
+    # add/update daily data
+    def putrow(self, myOriginalDT, mytemp_max, mytemp_min, mydescription):
+        mydt = myOriginalDT + self.varTimezone
+        mydtkey = datetime.utcfromtimestamp(mydt).strftime('%Y-%m-%d')  # strip time from stamp, just keep date
+
+        # new daily entry (judged by the cross-ref)
+        if mydtkey not in self.dicDay2IndexXF.keys():
+            # new entry on the cross-reference dictionary - key to list indexes
+            self.dicDay2IndexXF.update({mydtkey: self.cntNextIndex})
+
+            # weather attributes for the day
+            self.lstMaxT.insert(self.cntNextIndex, mytemp_max)
+            self.lstMinT.insert(self.cntNextIndex, mytemp_min)
+            self.lstLastWD.insert(self.cntNextIndex, mydescription)
+            self.lstFirstWD.insert(self.cntNextIndex, mydescription)
+
+            # Unix time stamps for each of those attributes
+            self.lstMaxTUtime.insert(self.cntNextIndex, mydt)
+            self.lstMinTUtime.insert(self.cntNextIndex, mydt)
+            self.lstLastWDUtime.insert(self.cntNextIndex, mydt)
+            self.lstFirstWDUtime.insert(self.cntNextIndex, mydt)
+
+            self.cntNextIndex += 1
+
+        # updade of current daily entry
+        else:
+            mylst_index = self.dicDay2IndexXF[mydtkey]
+
+            # update self temp max if lower than new for the day
+            if self.lstMaxT[mylst_index] < mytemp_max:
+                self.lstMaxT[mylst_index] = mytemp_max
+                self.lstMaxTUtime[mylst_index] = mydt
+
+            # update self temp min if higher than new for the day
+            if self.lstMinT[mylst_index] > mytemp_min:
+                self.lstMinT[mylst_index] = mytemp_min
+                self.lstMinTUtime[mylst_index] = mydt
+
+            # update self weather description if not first for the day
+            if self.lstFirstWDUtime[mylst_index] > mydt:
+                self.lstFirstWD[mylst_index] = mydescription
+                self.lstFirstWDUtime[mylst_index] = mydt
+
+            # update self weather description if not last for the day
+            if self.lstLastWDUtime[mylst_index] < mydt:
+                self.lstLastWD[mylst_index] = mydescription
+                self.lstLastWDUtime[mylst_index] = mydt
+        return
+
+    # Returns a list of 4 formatted data elements for report from the given date key
+    def getrow_report(self, mydtkey):
+
+        if mydtkey not in self.dicDay2IndexXF.keys():
+            return ["", 0, 0, "ERROR date not found: " + mydtkey]
+
+        else:
+            if self.lstFirstWD[self.dicDay2IndexXF[mydtkey]] == self.lstLastWD[self.dicDay2IndexXF[mydtkey]]:
+                myweather = self.lstFirstWD[self.dicDay2IndexXF[mydtkey]]
+            else:
+                myweather = self.lstFirstWD[self.dicDay2IndexXF[mydtkey]] + " to " + self.lstLastWD[
+                    self.dicDay2IndexXF[mydtkey]]
+
+            return [datetime.fromisoformat(mydtkey).strftime('%a'), round(self.lstMaxT[self.dicDay2IndexXF[mydtkey]]),
+                    round(self.lstMinT[self.dicDay2IndexXF[mydtkey]]), myweather]
+        return
+
+
+
 def isunixdt(inval):
     # return true if inval is numeric of 10 digits (validating unix dt)
     # this method will stop working properly on year 2038 because of signed 32 bit unix utc time issue
@@ -121,6 +221,37 @@ def get5fromjson(loaded_json, my5day, my5max, my5min, my5maxw, my5minw):  # popu
 
         my5minw.update({myday: x['weather'][0]['description']})
 
+    return
+
+def get5fromjson2(loaded_json, myweather):  # populate the instance weather class from JSON
+
+    # parse the city header data from json
+    for key, value in loaded_json['city'].items():
+        if key == 'name': myweather.varCityName = value
+        if key == 'country': myweather.varCountryName = value
+        if key == 'sunrise': myweather.varSunrise = value
+        if key == 'sunset': myweather.varSunset = value
+        if key == 'timezone': myweather.varTimezone = value
+
+    # adjust Unix times for timezone
+    myweather.varSunrise += myweather.varTimezone
+    myweather.varSunset += myweather.varTimezone
+
+    # get full name of the country (change ISO code to full common name)
+    mycountrycode = myweather.varCountryName
+    myweather.varCountryName = pycountry.countries.get(alpha_2=mycountrycode).name
+
+    # parse the Entire list data from json
+    for x in loaded_json['list']:
+        myOriginalDT = x['dt']
+        mytemp_max = x['main']['temp_max']
+        mytemp_min = x['main']['temp_min']
+        mydescription = x['weather'][0]['description']
+
+        # send thee info to the weather class to be further reviewed and stored into the day info
+        myweather.putrow(myOriginalDT, mytemp_max, mytemp_min, mydescription)
+
+    return
 
 def display5(my5day, my5max, my5min, my5maxw, my5minw):  # print the forecast report
     # Day High Temp Low Temp   Wather thought the day
@@ -154,6 +285,37 @@ def display5(my5day, my5max, my5min, my5maxw, my5minw):  # print the forecast re
         print("{0:4} {1:^9} {2:^9} {3}".format((datetime.fromisoformat(key).strftime('%a')), round(my5max[key]),
                                                round(my5min[key]), myweather))
 
+    return
+
+
+def display52(myweather):  # print the forecast report
+    # Day High Temp Low Temp   Wather thought the day
+    # --- --------- ---------- -----------------------------------------------
+    # \x1b[34m for blue
+    # \x1b[4m for underline
+    # \x1b[0m for no-color
+
+    # Headings
+    print(
+        "\x1b[34m\nForecast for \x1b[4m{0} ({1})\x1b[0m\x1b[34m - Temperature in Fahrenheit, Using Local {0} time.".format(
+            myweather.varCityName,
+            myweather.varCountryName))
+    print(
+        "Sunrise: {0}  Sunset: {1} for {2} \x1b[0m\n".format(
+            datetime.utcfromtimestamp(myweather.varSunrise).strftime('%I:%M %p'),
+            datetime.utcfromtimestamp(myweather.varSunset).strftime('%I:%M %p'),
+            datetime.utcfromtimestamp(myweather.varSunrise).strftime(
+                '%A %B %d, %Y')))
+
+    print("{0:4} {1:^9} {2:^9} {3}".format("Day", "High Temp", "Low Temp", "Weather throughout the day"))
+    print("-" * 4 + " " + "-" * 9 + " " + "-" * 9 + " " + "-" * 50)
+
+    # data expressed in sorted form (in case lson dates were not in order)
+    for key in sorted (myweather.dicDay2IndexXF.keys()):
+
+        print("{0:4} {1:^9} {2:^9} {3}".format(*myweather.getrow_report(key)))
+
+    return
 
 def main():
     my5day = dict()  # the dictionary for heading info (timezone,name,country,sunrise,sunset)
@@ -161,6 +323,7 @@ def main():
     my5min = dict()  # the dictionary for min day temp (dates are key)
     my5maxw = dict()  # the dictionary for max day weather description (dates are key)
     my5minw = dict()  # the dictionary for min day weather description (dates are key)
+
 
     myinput = "Y"
     while myinput in ['y', 'Y']:  # add to cart until user presses q/Q
@@ -210,16 +373,30 @@ def main():
         my5min.clear()
         my5maxw.clear()
         my5minw.clear()
+
+        if 'myweather' in locals():  #delete the instance of clsWeather if already exists
+            del myweather
+        myweather = clsWeather()
+
         get5fromjson(loaded_json, my5day, my5max, my5min, my5maxw, my5minw)
 
-        # print(my5day)
-        # print(my5max)
-        # print(my5min)
-        # print(my5maxw)
-        # print(my5minw)
+        get5fromjson2(loaded_json, myweather)
+
+        print(my5day)
+        print(myweather.dicDay2IndexXF)
+        print(my5max)
+        print(myweather.lstMaxT)
+        print(my5min)
+        print(myweather.lstMinT)
+        print(my5maxw)
+        print(myweather.lstFirstWD)
+        print(my5minw)
+        print(myweather.lstLastWD)
 
         # print the weather forecast
         display5(my5day, my5max, my5min, my5maxw, my5minw)
+
+        display52(myweather)
 
         myinput = input("\nExtract another forecast? (y/Y for YES, any other to QUIT): ")
 
